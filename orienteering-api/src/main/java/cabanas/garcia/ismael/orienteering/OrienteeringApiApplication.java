e62bcbf66e6603a3c6cb47f9fb3933e76fbaa825
Ismael Cabañas Garcia <icabanas@ext.vectorsf.com>
2016-04-01 12:23:20 +0200

    Creación del proyecto

---
+++ orienteering-api/src/main/java/cabanas/garcia/ismael/orienteering/OrienteeringApiApplication.java
@@ -0,0 +1,12 @@
+package cabanas.garcia.ismael.orienteering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrienteeringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrienteeringApiApplication.class, args);
+	}
+}
---
+++ orienteering-api/src/test/java/cabanas/garcia/ismael/orienteering/OrienteeringApiApplicationTests.java
@@ -0,0 +1,18 @@
+package cabanas.garcia.ismael.orienteering;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = OrienteeringApiApplication.class)
+@WebAppConfiguration
+public class OrienteeringApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
